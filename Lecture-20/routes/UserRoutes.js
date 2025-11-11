4a0b202513f7f107a19d55fc432d3cf9480a3eda
Sneha Kundu <157353862+snehak-05@users.noreply.github.com>
2025-11-11 09:58:58 +0530

    Create UserRoutes.js

---
+++ Lecture-20/routes/UserRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const {m5} = require("../middleware/RouterLevel")
+router.use(m5)
+
+router.post("/", (req, res) => {
+  res.json ({
+    success : true,
+    message : "User added successfully"
+  })
+})
+
+router.get("/", (req, res) => {
+  res.json ({
+    success : true,
+    message : "All users data fetched successfully"
+  })
+})
+
+router.get("/:id", (req, res) => {
+  res.json ({
+    success : true,
+    message : "Single user data fetched"
+  })
+})
+
+module.exports = router
